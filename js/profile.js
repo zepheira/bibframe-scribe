7223e1b501e3a354fc9a6b74deb905f14ced8c41
Ryan Lee <ryanlee@zepheira.com>
2013-10-15 21:10:20 -0700

    write up profile class

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,43 @@
-var Profile = function(obj) {
-    this._id = null;
-    this._resourceTemplates = [];
+var Profile = function(id, obj) {
+    var rt, i, template;
+
+    this._id = id;
+    this._resourceTemplates = {};
+    this._idToTemplate = {};
+
+    if (typeof obj.resourceTemplate !== "undefined") {
+        for (i = 0; i < obj.resourceTemplate.length; i++) {
+            template = obj.resourceTemplate[i];
+            rt = new ResourceTemplate(template);
+            this._resourceTemplates[rt.getClassID()] = rt;
+            this._idToTemplate[rt.getID()] = rt;
+        }
+    }
 };
 
-// @@@ write this up
+Profile.prototype.getID = function() {
+    return this._id;
+};
+
+Profile.prototype.getResourceTemplate = function(id) {
+    if (typeof this._resourceTemplates[id] !== "undefined") {
+        return this._resourceTemplates[id];
+    } else {
+        return null;
+    }
+};
+
+Profile.prototype.getTemplateByID = function(id) {
+    if (typeof this._idToTemplate[id] !== "undefined") {
+        return this._idToTemplate[id];
+    }
+};
+
+Profile.prototype.registerResourceTemplates = function(map) {
+    var id;
+    for (id in this._resourceTemplates) {
+        if (this._resourceTemplates.hasOwnProperty(id)) {
+            map[this._resourceTemplates[id].getID()] = this._resourceTemplates[id];
+        }
+    }
+};
