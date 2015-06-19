178b8f78ead652942012f5a137e99712a734a3a7
Ryan Lee <ryanlee@zepheira.com>
2015-06-19 16:59:25 -0700

    add karma conf for jenkins CI

---
+++ .karma-jenkins.conf.js
@@ -0,0 +1,50 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      'app/static/js/lib/angular.min.js',
+      'app/static/js/lib/angular-resource.min.js',
+      'app/static/js/lib/ui-bootstrap-tpls-0.13.1-SNAPSHOT.min.js',
+      'app/static/js/lib/http-throttler.js',
+      'app/static/js/lib/dropzone.min.js',
+      'app/static/js/lib/rdf_store_min.js',
+      'tests/angular-mocks.js',
+      'app/static/js/profile.js',
+      'app/static/js/value-constraint.js',
+      'app/static/js/property.js',
+      'app/static/js/property-template.js',
+      'app/static/js/resource-template.js',
+      'app/static/js/pred-object.js',
+      'app/static/js/namespace.js',
+      'app/static/js/services.js',
+      'app/static/js/initializer.js',
+      'app/static/js/controllers.js',
+      'tests/unit/*.js'
+    ],
+
+    exclude: [
+      'app/static/js/lib/angular.js'
+    ],
+
+    reporters: ['dots', 'junit'],
+
+    port: 9876,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: false,
+
+    browsers: ['PhantomJS'],
+
+    singleRun: true,
+
+    junitReporter = {
+      outputFile: 'test-results.xml'
+    };
+  });
+};
