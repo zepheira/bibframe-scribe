6ff9b79264cd35381e6d37031828bb745ad46e03
Ryan Lee <ryanlee@zepheira.com>
2015-06-22 16:25:43 -0700

    increse coverage

--- tests/unit/property.js
+++ tests/unit/property.js
@@ -1,16 +1,33 @@
 "use strict";
 
 describe("Property", function() {
-    var prop;
-    beforeEach(function() {
-        prop = new Property({"id": "test", "propertyLabel": "Test"});
-    });
+    describe("constructor", function() {
+        var prop;
+        beforeEach(function() {
+            prop = new Property({"id": "test", "propertyLabel": "Test"});
+        });
     
-    it("should have the right ID", function() {
-        expect(prop.getID()).toEqual("test");
+        it("should have the right ID", function() {
+            expect(prop.getID()).toEqual("test");
+        });
+        
+        it("should have the right label", function() {
+            expect(prop.getLabel()).toEqual("Test");
+        });
     });
 
-    it("should have the right label", function() {
-        expect(prop.getLabel()).toEqual("Test");
-    });
+    describe("empty constructor", function() {
+        var prop;
+        beforeEach(function() {
+            prop = new Property({});
+        });
+    
+        it("should have a null ID", function() {
+            expect(prop.getID()).toEqual(null);
+        });
+        
+        it("should have a null label", function() {
+            expect(prop.getLabel()).toEqual(null);
+        });
+    });    
 });
