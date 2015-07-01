340e4eb04ce6004c30c6ed1dca71a8feec589151
Ryan Lee <ryanlee@zepheira.com>
2015-06-30 21:00:55 -0700

    test fails in phantomjs, catch and attempt to adapt if detected

--- tests/unit/initializer.js
+++ tests/unit/initializer.js
@@ -45,5 +45,32 @@ describe("BIBFRAME Editor application", function() {
                 expect(elm.text()).toEqual("test1");
             }
         });
+
+        it("should evaluate on Enter key press", function() {
+            var e, pjs = false;
+            expect(elm.text()).toEqual("test{{a}}");
+            $scope.$digest();
+            expect($scope.a).toEqual(0);
+            expect(elm.text()).toEqual("test0");
+            // Keyboard stuff is so jacked up across browsers that this
+            // test will fail in Chrome and Safari, and PhantomJS while
+            // running fine in Firefox and IE; the implementation should
+            // be sufficient to work cross-browser, but it's not worth the
+            // trouble to make the test work cross-browser.
+            try {
+                e = new KeyboardEvent("keypress", {code: "Enter", key: "Enter", location: 0, which: 13});
+            } catch (e) {
+                pjs = true;
+                e = document.createEvent("KeyboardEvent");
+                e.initEvent("keypress", true, false);
+                e.keyCode = 13;
+            }
+            if (typeof e.code !== "undefined" || pjs) {
+                elm[0].dispatchEvent(e);
+                $scope.$digest();
+                expect($scope.a).toEqual(1);
+                expect(elm.text()).toEqual("test1");
+            }
+        });
     });
 });
