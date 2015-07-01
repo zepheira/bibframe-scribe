c947e351c0b903b4f0f4b5fa8ddc5c1e175be68e
Ryan Lee <ryanlee@zepheira.com>
2015-06-30 22:50:08 -0700

    currently unused; adding code to handle working with a resource instead of embedding it in the controller

---
+++ app/static/js/factories/resource.js
@@ -0,0 +1,280 @@
+(function() {
+    angular
+        .module("resourceFactory", ["predObjectFactory", "namespaceService"])
+        .factory("Resource", ResourceFactory);
+
+    function ResourceFactory(PredObject, Namespace) {
+        function Resource(id, tmpl) {
+            this._id = id;
+            this._template = tmpl;
+            this._type = tmpl.getClassID();
+            this._properties = {};
+            this._pristine = {};
+        }
+        
+        /**
+         * Return resource URI.
+         */
+        Resource.prototype.getID = function() {
+                return this._id;
+        };
+
+        /**
+         * Return resource URI.
+         */
+        Resource.prototype.getType = function() {
+            return this._type;
+        };
+        
+        /**
+         * Initialize hash for new property in resource.
+         */
+        Resource.prototype.initializeProperty = function(property, flags) {
+            var prop, constraint, defaultVal;
+            prop = property.getProperty().getID();
+            if (typeof this._properties[prop] === "undefined") {
+                this._properties[prop] = [];
+                this._pristine[prop] = [];
+            }
+            if (property.hasConstraint()) {
+                constraint = property.getConstraint();
+                if (constraint.hasDefaultURI()) {
+                    if (constraint.hasDefaultLiteral()) {
+                        defaultVal = new PredObject(constraint.getDefaultLiteral(), constraint.getDefaultURI(), property.getType(), false);
+                    } else {
+                        defaultVal = new PredObject(constraint.getDefaultURI(), constraint.getDefaultURI(), property.getType(), false);
+                    }
+                    this._properties[prop].push(defaultVal);
+                    this._pristine[prop].push(defaultVal);
+                } else if (property.getConstraint().hasDefaultLiteral()) {
+                    defaultVal = new PredObject(constraint.getDefaultLiteral(), null, property.getType(), false);
+                    this._properties[prop].push(defaultVal);
+                    this._pristine[prop].push(defaultVal);
+                }
+            }
+            if (!flags.hasRequired && property.isRequired()) {
+                flags.hasRequired = true;
+            }
+            flags.loading[prop] = false;
+        };
+        
+        /**
+         * Set work's value of property to String or Date value.
+         * Returns true if it makes a change in the model, false if not.
+         */
+        Resource.prototype.addPropertyValue = function(property, value) {
+            var propID, seen, val, isDirty, textValue;
+            isDirty = false;
+            propID = property.getProperty().getID();
+            objType = property.getType();
+            seen = false;
+            if (typeof value === "object" && typeof value.toISOString !== "undefined") {
+                textValue = value.toISOString().split("T")[0];
+            } else {
+                textValue = value;
+            }
+            angular.forEach(this._properties[propID], function(val) {
+                if (val.getValue() === textValue) {
+                    seen = true;
+                }
+            });
+            if (!seen && textValue !== "") {
+                isDirty = true;
+                val = new PredObject(textValue, textValue, objType, true);
+                if (property.hasConstraint() && property.getConstraint().hasComplexType()) {
+                    val.setDatatype(property.getConstraint().getComplexTypeID());
+                };
+                this._properties[propID].push(val);
+            }
+            return isDirty;
+        };
+        
+        /**
+         * Delete a value for a work's property, return whether a change
+         * in the model occurred or not.
+         */
+        Resource.prototype.removePropertyValue = function(property, value) {
+            var prop, empty, objs, rmIdx;
+            removed = false;
+            if (typeof property === "string") {
+                prop = property;
+            } else {
+                prop = property.getProperty().getID();
+            }
+            objs = this._properties[prop];
+            rmIdx = -1;
+            angular.forEach(objs, function(obj, idx) {
+                if (obj.getValue() === value.getValue()) {
+                    rmIdx = idx;
+                }
+            });
+            if (rmIdx >= 0) {
+                objs.splice(rmIdx, 1);
+                removed = true;
+            }
+            return removed;
+        };
+        
+        /**
+         * Return true if model has no modifications from pristine state.
+         */
+        Resource.prototype.isEmpty = function() {
+            var empty = true,
+            res = this;
+            angular.forEach(this._properties, function(vals, prop) {
+                if (empty && res._properties[prop].length === res._pristine[prop].length && res._pristine[prop].length > 0) {
+                    angular.forEach(vals, function(val, i) {
+                        if (res._pristine[prop][i] !== val) {
+                            empty = false;
+                        }
+                    });
+                }
+            });
+            return empty;
+        };
+        
+        /**
+         * Remove all user modifications.
+         */
+        Resource.prototype.reset = function() {
+            angular.forEach(this._properties, function(vals, prop) {
+                this._properties[prop] = this._pristine[prop];
+            });
+        };
+        
+        /**
+         * Export resource as an RDF/XML string.
+         */
+        Resource.prototype._resourceToRDF = function(refs) {
+            var frag = "",
+            result = "",
+            relFrag = "",
+            res = this,
+            type = this.getType(),
+            id = this.getID(),
+            split = false,
+            relation;
+            
+            if (res._template !== null) {
+                relation = res._template.getRelation();
+                if (relation !== null) {
+                    split = true;
+                }
+            }
+            angular.forEach(res._properties, function(vals, prop) {
+                var nsProp;
+                if (prop === "id" && id === null) {
+                    id = vals;
+                } else if (prop === "type" && type === null) {
+                    type = vals.getValue();
+                } else {
+                    nsProp = Namespace.extractNamespace(prop);
+                    if ((split && res._template.hasProperty(prop)) || !split) {
+                        angular.forEach(vals, function(val) {
+                            if (val.isResource()) {
+                                frag += '    <' + nsProp.namespace + ':' + nsProp.term + ' rdf:resource="' + val.getValue() + '"/>\n';
+                                if (typeof refs !== "undefined") {
+                                    refs.push(val.getValue());
+                                }
+                            } else {
+                                frag += '    <'+ nsProp.namespace + ':' + nsProp.term;
+                                if (val.hasDatatype()) {
+                                    frag += ' rdf:datatype="' + val.getDatatype() + '"';
+                                }
+                                frag += '>' + val.getValue() + '</' + nsProp.namespace  + ':' + nsProp.term + '>\n';
+                            }
+                        });
+                    } else {
+                        angular.forEach(vals, function(val) {
+                            if (val.isResource()) {
+                                relFrag += '    <' + nsProp.namespace + ':' + nsProp.term + ' rdf:resource="' + val.getValue() + '"/>\n';
+                                if (typeof refs !== "undefined") {
+                                    refs.push(val.getValue());
+                                }
+                            } else {
+                                relFrag += '    <'+ nsProp.namespace + ':' + nsProp.term;
+                                if (val.hasDatatype()) {
+                                    relFrag += ' rdf:datatype="' + val.getDatatype() + '"';
+                                }
+                                relFrag += '>' + val.getValue() + '</' + nsProp.namespace  + ':' + nsProp.term + '>\n';
+                            }
+                        });
+                    }
+                }
+            });
+            if (split) {
+                nsProp = Namespace.extractNamespace('http://bibframe.org/vocab/instanceOf'); // @@@ faking it
+                frag += '    <' + nsProp.namespace + ':' + nsProp.term + ' rdf:resource="' + id + '-work" />\n';
+                result += '  <rdf:Description rdf:about="' + id + '-work">\n    <rdf:type rdf:resource="' + relation + '"/>\n' + relFrag + '  </rdf:Description>\n';
+            }
+            result += '  <rdf:Description rdf:about="' + id + '">\n    <rdf:type rdf:resource="' + type + '"/>\n' + frag + '  </rdf:Description>\n';
+            return result;
+        };
+            
+        Resource.prototype.toRDF = function(created, prefixed) {
+            var rdf, head, tail, refs;
+            rdf = '';
+            tail = '</rdf:RDF>\n';
+            refs = [];
+            rdf += this._resourceToRDF(refs);
+            angular.forEach(created, function(res) {
+                if (refs.indexOf(res.getID()) >= 0) {
+                    rdf += res.toRDF([], false);
+                }
+            });
+            if (prefixed) {
+                head = '<?xml version="1.0"?>\n\n' + Namespace.buildRDF() + '\n';
+                rdf = head + rdf + tail;
+            }
+            return rdf;
+        };
+        
+        Resource.prototype._resourceToN3 = function(refs) {
+            var frag = "",
+            res = this;
+            angular.forEach(this._properties, function(vals, prop) {
+                if (prop === "id" && typeof res.getID() === "undefined") {
+                    res._id = vals;
+                } else if (prop === "type" && typeof res.getType() === "undefined") {
+                    res._type = vals.getValue();
+                } else {
+                    angular.forEach(vals, function(val) {
+                        if (val.isResource()) {
+                            frag += '  <' + prop + '> <' + val.getValue() + '>;\n';
+                            if (typeof refs !== "undefined") {
+                                refs.push(val.getValue());
+                            }
+                        } else {
+                            frag += '  <' + prop + '> \"' + val.getValue() + '\"';
+                            if (val.hasDatatype()) {
+                                frag += '^^<' + val.getDatatype() + '>';
+                            }
+                            frag += ';';
+                        }
+                    });
+                }
+            });
+            return '<' + this.getID() + '>\n' + frag + ' rdf:type <' + this.getType() + '> .\n';
+        };
+
+        Resource.prototype.toN3 = function(created, prefixed) {
+            var rdf, refs, res;
+            if (typeof created === "undefined" || created === null) {
+                created = [];
+            }
+            
+            rdf = prefixed ? '@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\n' : "";
+            refs = [];
+            rdf += this._resourceToN3(refs);
+            angular.forEach(created, function(res) {
+                if (refs.indexOf(res.getID()) >= 0) {
+                    rdf += res.toN3([], false);
+                }
+            });
+            
+            return rdf;
+        };
+
+        return Resource;
+    }
+})();
