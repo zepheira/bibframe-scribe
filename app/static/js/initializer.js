5e012902b18c1e7c9c1cdff14a3a56fd8b69a105
Ryan Lee <ryanlee@zepheira.com>
2015-06-30 20:01:09 -0700

    be compatible with more modern DOM levels, too

--- app/static/js/initializer.js
+++ app/static/js/initializer.js
@@ -20,7 +20,7 @@ angular.module("bibframeEditor", [
 }]).directive("ngEnter", function() {
     return function(scope, element, attrs) {
         element.bind("keydown keypress", function(event) {
-            if (event.which === 13) {
+            if ((event.code && event.code === "Enter") || (event.which && event.which === 13) || (event.keyCode && event.keyCode === 13)) {
                 scope.$apply(function() {
                     scope.$eval(attrs.ngEnter);
                 });
