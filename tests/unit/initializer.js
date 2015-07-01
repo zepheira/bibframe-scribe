49f3ab2f966952d292ab0b77a994993f29ec1937
Ryan Lee <ryanlee@zepheira.com>
2015-06-30 20:00:08 -0700

    add missing semi; test directive, with notes on how the test isn't cross-browser because that's a test more complex than the function it's testing

--- tests/unit/initializer.js
+++ tests/unit/initializer.js
@@ -6,7 +6,7 @@ describe("BIBFRAME Editor application", function() {
     var $scope, $controller;
 
     beforeEach(inject(function($rootScope, _$controller_) {
-        $scope = $rootScope.$new()
+        $scope = $rootScope.$new();
         $controller = _$controller_;
     }));
 
@@ -14,6 +14,36 @@ describe("BIBFRAME Editor application", function() {
         it("should exist", function() {
             var editorCtrl = $controller("EditorCtrl", {$scope: $scope});
             expect(editorCtrl).toBeDefined();
+        });
+    });
+
+    describe("ngEnter attribute directive", function() {
+        var elm;
+        beforeEach(inject(function($compile) {
+            $scope.a = 0;
+            $scope.setIt = function() {
+                ++$scope.a;
+            }
+            elm = angular.element("<span ng-enter='setIt()'>test{{a}}</span>");
+            $compile(elm)($scope);
+        }));
+
+        it("should evaluate on Enter key down", function() {
+            expect(elm.text()).toEqual("test{{a}}");
+            $scope.$digest();
+            expect($scope.a).toEqual(0);
+            expect(elm.text()).toEqual("test0");
+            var e = new KeyboardEvent("keydown", {code: "Enter", key: "Enter", location: 0, which: 13});
+            // Keyboard stuff is so jacked up across browsers that this
+            // test will fail in Chrome and Safari; the implementation should
+            // be sufficient to work cross-browser, but it's not worth the
+            // trouble to make the test work cross-browser.
+            if (typeof e.code !== "undefined") {
+                elm[0].dispatchEvent(e);
+                $scope.$digest();
+                expect($scope.a).toEqual(1);
+                expect(elm.text()).toEqual("test1");
+            }
         });
     });
 });
