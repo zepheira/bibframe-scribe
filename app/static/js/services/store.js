9d98c92be9debd80fb63637f7558b5f7878e9663
Ryan Lee <ryanlee@zepheira.com>
2015-07-16 16:21:08 -0700

    adapt service to new server-side spec

--- app/static/js/services/store.js
+++ app/static/js/services/store.js
@@ -10,7 +10,7 @@
             {
                 "id": {
                     "method": "POST",
-                    "isArray": false,
+                    "isArray": true,
                     "params": {
                         "action": "id"
                     }
